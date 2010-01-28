d747cae6f696005c12782842aae624088225f6db
Pascal Friederich <pascal.friederich@xing.com>
2010-01-28 15:46:32 +0100

    moved subscribe and listen code/tests to subscriber

--- test/bandersnatch/base_test.rb
+++ test/bandersnatch/base_test.rb
@@ -92,55 +92,6 @@ module Bandersnatch
     end
   end
 
-
-  class SubscriptionTest < Test::Unit::TestCase
-    def setup
-      @bs = Base.new
-    end
-
-    test "subscribe should create subscriptions for all servers" do
-      @bs.servers << "localhost:7777"
-      @bs.messages = {"a" => 1, "b" => 2}
-      @bs.expects(:subscribe_message).with("a").times(2)
-      @bs.expects(:subscribe_message).with("b").times(2)
-      @bs.subscribe
-    end
-
-    test "subscribe_message should subscribe with a subscription callback created from the registered block" do
-      opts = {"ack" => true, "key" =>"some_key"}
-      server = @bs.server
-      header = mock("header")
-      header.expects(:ack)
-      block_called = false
-      proc = lambda do |m|
-        block_called = true
-        assert_equal header, m.header
-        assert_equal "data", m.data
-        assert_equal server, m.server
-        assert_nil m.uuid
-      end
-      @bs.register_handler("some_message", opts, &proc)
-      q = mock("QUEUE")
-      q.expects(:subscribe).with({:ack => true, :key => "some_key.#"}).yields(header, Message.encode("data"))
-      @bs.expects(:queues).returns({"some_message" => q})
-      @bs.subscribe_message("some_message")
-      assert block_called
-    end
-
-    test "subscribe should fail if no handler exists for given message" do
-      assert_raises(Error){ @bs.subscribe_message("some_message") }
-    end
-
-    test "listening should use eventmachine. create exchanges. bind queues. install subscribers." do
-      EM.expects(:run).yields
-      @bs.expects(:create_exchanges).with(["a"])
-      @bs.expects(:bind_queues).with(["a"])
-      @bs.expects(:subscribe)
-      @bs.listen(["a"]) {}
-    end
-
-  end
-
   class ServerManagementTest < Test::Unit::TestCase
     def setup
       @bs = Base.new
@@ -198,72 +149,6 @@ module Bandersnatch
       @bs.recycle_dead_servers
       assert_equal ["b:2", "a:1"], @bs.servers
       assert_equal({}, dead)
-    end
-  end
-
-  class DeDuplificationTest < Test::Unit::TestCase
-    def setup
-      @bs = Base.new
-      @handler = mock("handler")
-      @queue = 'somequeue'
-      @callback = @bs.create_subscription_callback('servername', @queue, @handler)
-    end
-
-    test 'a message handler should call the callback if the message id is not already in the database' do
-      body = Message.encode('my message', :with_uuid => true)
-      header = mock("header")
-      header.expects(:ack)
-      message = Message.new("server", header, body)
-
-      Message.any_instance.expects(:insert_id).with(@queue).returns(true)
-      @handler.expects(:call)
-      @callback.call(header, body)
-    end
-
-    test "a message handler should not call the callback if the message id is already in the database" do
-      body = Message.encode("my message", :with_uuid => true)
-      header = mock("header")
-      message = Message.new("server", header, body)
-      header.expects(:ack)
-
-      @handler.expects(:call).never
-      Message.any_instance.expects(:insert_id).with(@queue).returns(false)
-      @callback.call(header, body)
-    end
-
-    test "the message callback should not get called and the message should not be ack'ed if the db check fails" do
-      body = Message.encode("my message", :with_uuid => true)
-      header = mock("header")
-      message = Message.new("server", header, body)
-
-      header.expects(:ack).never
-      Message.any_instance.expects(:insert_id).raises(Exception)
-      EM.expects(:add_timer).once
-      @callback.call(header, body)
-    end
-
-    test "the internal timer should get refreshed for every failing db check" do
-      body = Message.encode("my message", :with_uuid => true)
-      header = mock("header")
-      message = Message.new("server", header, body)
-      Message.any_instance.expects(:insert_id).raises(Exception)
-      timer = mock("timer")
-
-      timer.expects(:cancel).once
-      EM::Timer.expects(:new).twice.returns(timer)
-      @callback.call(header, body)
-      @callback.call(header, body)
-    end
-
-    test "expired messages should be silently dropped without inserting a uuid into the database" do
-      body = Message.encode("my message", :ttl => -1)
-      header = mock("header")
-      header.expects(:ack)
-      message = Message.new("server", header, body)
-
-      @handler.expects(:call).never
-      Message.any_instance.expects(:insert_id).never
-      @callback.call(header, body)
     end
   end
 end
