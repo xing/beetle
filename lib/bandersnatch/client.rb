cffb3dac9126f121b0152121525c6179ca65c53b
Pascal Friederich <pascal.friederich@xing.com>
2010-01-28 15:17:54 +0100

    memoize the publisher/subscriber instances

--- lib/bandersnatch/client.rb
+++ lib/bandersnatch/client.rb
@@ -1,29 +1,32 @@
 module Bandersnatch
   class Client
-    def initialize
-      # TODO: refactoring helper code, will be replaced
-      @publisher = Publisher.new
-      @subscriber = Subscriber.new
-    end
-
     # TODO: refactoring helper code, will be replaced
     def publish(message_name, data, opts={})
-      @publisher.publish(message_name, data, opts={})
+      publisher.publish(message_name, data, opts={})
     end
 
     # TODO: refactoring helper code, will be replaced
     def subscribe(messages = nil)
-      @subscribe.subscribe(messages)
+      subscribe.subscribe(messages)
     end
 
     # TODO: refactoring helper code, will be replaced
     def trace
-      @subscriber.trace
+      subscriber.trace
     end
 
     # TODO: refactoring helper code, will be replaced
     def test
-      @publisher.test
+      publisher.test
     end
+
+    private
+      def publisher
+        @publisher ||= Publisher.new
+      end
+
+      def subscriber
+        @subscriber ||= Subscriber.new
+      end
   end
 end
