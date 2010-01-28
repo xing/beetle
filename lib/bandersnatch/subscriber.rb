e59acbbc94390f568f5e7fe006294d643805ae35
Pascal Friederich <pascal.friederich@xing.com>
2010-01-28 15:46:32 +0100

    moved subscribe and listen code/tests to subscriber

--- lib/bandersnatch/subscriber.rb
+++ lib/bandersnatch/subscriber.rb
@@ -1,6 +1,69 @@
 module Bandersnatch
   class Subscriber < Base
+    def listen(messages=@messages.keys)
+      EM.run do
+        yield if block_given?
+        create_exchanges(messages)
+        bind_queues(messages)
+        subscribe
+      end
+    end
+
     private
+      def subscribe(messages=nil)
+        messages ||= @messages.keys
+        Array(messages).each do |message|
+          servers.each do |s|
+            set_current_server s
+            subscribe_message(message)
+          end
+        end
+      end
+
+      def subscribe_message(message)
+        handlers = Array(@handlers[message])
+        error("no handler for message #{message}") if handlers.empty?
+        handlers.each do |opts, block|
+          opts = opts.dup
+          key = opts.delete(:key) || message
+          queue = opts.delete(:queue) || message
+          callback = create_subscription_callback(@server, queue, block)
+          logger.debug "subscribing to queue #{queue} with key #{key} for message #{message}"
+          begin
+            queues[queue].subscribe(opts.merge(:key => "#{key}.#"), &callback)
+          rescue MQ::Error
+            error("Binding multiple handlers for the same queue isn't possible. You might want to use the :queue option")
+          end
+        end
+      end
+
+      def create_subscription_callback(server, queue, block)
+        lambda do |header,data|
+          begin
+            message = Message.new(server, header, data)
+
+            if message.expired?
+              logger.warn "Message expired: #{message.uuid}"
+            elsif message.insert_id(queue)
+              begin
+                block.call(message)
+              rescue Exception
+                logger.warn "Error during invocation of message handler for #{message}"
+              end
+            end
+
+            header.ack
+          rescue Exception
+            logger.error "Error during message processing. Message will get redelivered. #{message}\n #{$!}"
+            @timer.cancel if @timer
+            @timer = EM::Timer.new(RECOVER_AFTER) do
+              logger.info "Redelivering unacked messages that could not be verified because of unavailable Redis"
+              @mqs[server].recover(true)
+            end
+          end
+        end
+      end
+
       def create_exchange!(name, opts)
         mq.__send__(opts[:type], name, opts.slice(*EXCHANGE_CREATION_KEYS))
       end
--- test/bandersnatch/subscriber_test.rb
+++ test/bandersnatch/subscriber_test.rb
@@ -90,4 +90,117 @@ module Bandersnatch
       @sub.create_exchanges(messages)
     end
   end
+
+  class DeDuplificationTest < Test::Unit::TestCase
+    def setup
+      @sub = Subscriber.new
+      @handler = mock("handler")
+      @queue = 'somequeue'
+      @callback = @sub.send(:create_subscription_callback, 'servername', @queue, @handler)
+    end
+
+    test 'a message handler should call the callback if the message id is not already in the database' do
+      body = Message.encode('my message', :with_uuid => true)
+      header = mock("header")
+      header.expects(:ack)
+      message = Message.new("server", header, body)
+
+      Message.any_instance.expects(:insert_id).with(@queue).returns(true)
+      @handler.expects(:call)
+      @callback.call(header, body)
+    end
+
+    test "a message handler should not call the callback if the message id is already in the database" do
+      body = Message.encode("my message", :with_uuid => true)
+      header = mock("header")
+      message = Message.new("server", header, body)
+      header.expects(:ack)
+
+      @handler.expects(:call).never
+      Message.any_instance.expects(:insert_id).with(@queue).returns(false)
+      @callback.call(header, body)
+    end
+
+    test "the message callback should not get called and the message should not be ack'ed if the db check fails" do
+      body = Message.encode("my message", :with_uuid => true)
+      header = mock("header")
+      message = Message.new("server", header, body)
+
+      header.expects(:ack).never
+      Message.any_instance.expects(:insert_id).raises(Exception)
+      EM.expects(:add_timer).once
+      @callback.call(header, body)
+    end
+
+    test "the internal timer should get refreshed for every failing db check" do
+      body = Message.encode("my message", :with_uuid => true)
+      header = mock("header")
+      message = Message.new("server", header, body)
+      Message.any_instance.expects(:insert_id).raises(Exception)
+      timer = mock("timer")
+
+      timer.expects(:cancel).once
+      EM::Timer.expects(:new).twice.returns(timer)
+      @callback.call(header, body)
+      @callback.call(header, body)
+    end
+
+    test "expired messages should be silently dropped without inserting a uuid into the database" do
+      body = Message.encode("my message", :ttl => -1)
+      header = mock("header")
+      header.expects(:ack)
+      message = Message.new("server", header, body)
+
+      @handler.expects(:call).never
+      Message.any_instance.expects(:insert_id).never
+      @callback.call(header, body)
+    end
+  end
+
+  class SubscriptionTest < Test::Unit::TestCase
+    def setup
+      @sub = Subscriber.new
+    end
+
+    test "subscribe should create subscriptions for all servers" do
+      @sub.servers << "localhost:7777"
+      @sub.messages = {"a" => 1, "b" => 2}
+      @sub.expects(:subscribe_message).with("a").times(2)
+      @sub.expects(:subscribe_message).with("b").times(2)
+      @sub.send(:subscribe)
+    end
+
+    test "subscribe_message should subscribe with a subscription callback created from the registered block" do
+      opts = {"ack" => true, "key" =>"some_key"}
+      server = @sub.server
+      header = mock("header")
+      header.expects(:ack)
+      block_called = false
+      proc = lambda do |m|
+        block_called = true
+        assert_equal header, m.header
+        assert_equal "data", m.data
+        assert_equal server, m.server
+        assert_nil m.uuid
+      end
+      @sub.register_handler("some_message", opts, &proc)
+      q = mock("QUEUE")
+      q.expects(:subscribe).with({:ack => true, :key => "some_key.#"}).yields(header, Message.encode("data"))
+      @sub.expects(:queues).returns({"some_message" => q})
+      @sub.send(:subscribe_message, "some_message")
+      assert block_called
+    end
+
+    test "subscribe should fail if no handler exists for given message" do
+      assert_raises(Error){ @sub.send(:subscribe_message, "some_message") }
+    end
+
+    test "listening should use eventmachine. create exchanges. bind queues. install subscribers." do
+      EM.expects(:run).yields
+      @sub.expects(:create_exchanges).with(["a"])
+      @sub.expects(:bind_queues).with(["a"])
+      @sub.expects(:subscribe)
+      @sub.listen(["a"]) {}
+    end
+  end
 end
