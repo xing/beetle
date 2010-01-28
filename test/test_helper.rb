efef78b24c7ed703e90a3d273b54c9b29268e875
Pascal Friederich <paukul@gmail.com>
2010-01-28 11:26:58 +0100

    I like turtlez and colorfull rainbows (using redgreen)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'active_support'
 require 'active_support/testing/declarative'
 require 'test/unit'
+require 'redgreen' rescue nil
 require 'mocha'
 require File.expand_path(File.dirname(__FILE__) + '/../lib/bandersnatch')
 
