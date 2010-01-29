96c7f4d7d3e29d81d923c719d19b60f712bcd3d9
Pascal Friederich <pascal.friederich@xing.com>
2010-01-29 12:06:17 +0100

    default values to the configuration class instead of a block in the bandersnatch module

--- lib/bandersnatch/configuration.rb
+++ lib/bandersnatch/configuration.rb
@@ -1,5 +1,9 @@
 module Bandersnatch
   class Configuration
     attr_accessor :logger, :config_file, :environment
+    def initialize
+      self.logger = Logger.new(STDOUT)
+      self.environment = ENV['RAILS_ENV'] || "development"
+    end
   end
 end
