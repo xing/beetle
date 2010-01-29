d269104383a11d8d0ebd929015e28765aa0bd6bb
Stefan Kaes <skaes@railsexpress.de>
2010-01-29 09:27:40 +0100

    get rid of constant RAILS_ENV

--- lib/bandersnatch/configuration.rb
+++ lib/bandersnatch/configuration.rb
@@ -1,5 +1,9 @@
 module Bandersnatch
   class Configuration
-    attr_accessor :logger, :config_file
+    attr_accessor :logger, :config_file, :environment
+
+    def environment
+      @environment ||= ENV['RAILS_ENV'] || "development"
+    end
   end
 end
