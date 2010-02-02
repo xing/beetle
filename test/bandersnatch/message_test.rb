a7d0c5e66d4c596dcab71515bc7b995010fbfd83
Stefan Kaes <skaes@railsexpress.de>
2010-02-02 16:47:38 +0100

    more tests (and some bug fixes)

--- test/bandersnatch/message_test.rb
+++ test/bandersnatch/message_test.rb
@@ -118,7 +118,7 @@ module Bandersnatch
       assert_equal nil, @r.get(message.ack_count_key)
       message.process(lambda {|*args|})
       assert message.redundant?
-      assert_equal 1, @r.get(message.ack_count_key).to_i
+      assert_equal "1", @r.get(message.ack_count_key)
     end
 
     test "acking a redundant message twice should remove the ack_count key" do
@@ -201,6 +201,136 @@ module Bandersnatch
       message.send(:process_internal, proc)
     end
 
+    test "a retriable, redundant, fresh message should be acked after running the handler, and the ack count should be 1, and the status should be completed" do
+      body = Message.encode('my message', :with_uuid => true)
+      header = mock("header")
+      message = Message.new("somequeue", header, body)
+      message.retriable = true
+      message.timeout = 10.seconds
+      assert message.retriable?
+      assert message.redundant?
+
+      proc = mock("proc")
+      s = sequence("s")
+      proc.expects(:call).in_sequence(s)
+      message.expects(:completed!).in_sequence(s)
+      header.expects(:ack).in_sequence(s)
+      message.__send__(:process_internal, proc)
+      assert_equal "1", @r.get(message.ack_count_key)
+    end
+
+    test "a retriable, redundant, fresh message should not be acked if running the handler crashes, the status should be incomplete and the timeout should be 0" do
+      body = Message.encode('my message', :with_uuid => true)
+      header = mock("header")
+      message = Message.new("somequeue", header, body)
+      message.retriable = true
+      message.timeout = 10.seconds
+      assert message.retriable?
+      assert message.redundant?
+
+      proc = lambda {|*args| raise "crash"}
+      s = sequence("s")
+      message.expects(:completed!).never
+      header.expects(:ack).never
+      assert_raises(HandlerCrash) { message.__send__(:process_internal, proc) }
+      assert message.timed_out?
+      assert !message.completed?
+    end
+
+    test "a retriable, redundant, existing message should be just acked if the status is complete" do
+      body = Message.encode('my message', :with_uuid => true)
+      header = mock("header")
+      message = Message.new("somequeue", header, body)
+      message.retriable = true
+      message.timeout = 10.seconds
+      assert message.retriable?
+      assert message.redundant?
+      message.completed!
+
+      proc = mock("proc")
+      s = sequence("s")
+      header.expects(:ack)
+      proc.expects(:call).never
+      message.__send__(:process_internal, proc)
+      assert message.timed_out?
+    end
+
+    test "a retriable, redundant, existing, incomplete, timed out message should be processed again" do
+      body = Message.encode('my message', :with_uuid => true)
+      header = mock("header")
+      message = Message.new("somequeue", header, body)
+      message.retriable = true
+      message.timeout = 0
+      assert !message.key_exists?
+      assert message.key_exists?
+      assert message.timed_out?
+      assert message.retriable?
+      assert message.redundant?
+      assert !message.completed?
+
+      proc = mock("proc")
+      s = sequence("s")
+      proc.expects(:call).in_sequence(s)
+      header.expects(:ack).in_sequence(s)
+      message.__send__(:process_internal, proc)
+      assert message.completed?
+    end
+
+    test "a retriable, redundant, existing, incomplete, not yet timed out message should be processed later" do
+      body = Message.encode('my message', :with_uuid => true)
+      header = mock("header")
+      message = Message.new("somequeue", header, body)
+      message.retriable = true
+      message.timeout = Time.now.to_i + 10.seconds
+      message.set_timeout!
+      assert !message.key_exists?
+      assert message.key_exists?
+      assert !message.timed_out?
+      assert message.retriable?
+      assert message.redundant?
+      assert !message.completed?
+
+      proc = mock("proc")
+      proc.expects(:call).never
+      header.expects(:ack).never
+      assert_raises(HandlerTimeout){ message.__send__(:process_internal, proc) }
+      assert !message.completed?
+    end
+
   end
 
+  class ProcessingTest < Test::Unit::TestCase
+
+    test "processing a message catches exceptions risen by process_internal and reraises them" do
+      body = Message.encode('my message', :with_uuid => true)
+      header = mock("header")
+      message = Message.new("somequeue", header, body)
+      e = Exception.new
+      e2 = nil
+      message.expects(:process_internal).raises(e)
+      begin
+        message.process(1)
+      rescue Exception => e2
+      end
+      assert_equal e, e2
+    end
+  end
+
+  class SettingCompletionStatusTest < Test::Unit::TestCase
+    def setup
+      @r = Message.redis
+      @r.flushdb
+    end
+
+    test "completed! should store the status 'complete' in the database" do
+      body = Message.encode('my message', :with_uuid => true)
+      header = mock("header")
+      message = Message.new("somequeue", header, body)
+      assert !message.completed?
+      message.completed!
+      assert message.completed?
+      assert_equal "completed", @r.get(message.status_key)
+    end
+  end
 end
+
