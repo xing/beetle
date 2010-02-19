a78a1e301832569e34bf9a513666697c9a7515a8
Stefan Kaes <skaes@railsexpress.de>
2010-02-19 10:37:57 +0100

    yml config is gone

--- lib/beetle/configuration.rb
+++ lib/beetle/configuration.rb
@@ -1,10 +1,12 @@
 module Beetle
   class Configuration
-    attr_accessor :logger, :config_file, :environment, :gc_threshold
+    attr_accessor :logger, :gc_threshold, :redis_host, :redis_db, :servers
     def initialize
       self.logger = Logger.new(STDOUT)
-      self.environment = ENV['RAILS_ENV'] || "development"
       self.gc_threshold = 3.days
+      self.redis_host = "localhost"
+      self.redis_db = 4
+      self.servers = "localhost:5672"
     end
   end
 end
