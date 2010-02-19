f574a87ce349d5e4b63fc023f253b2e2d250e479
Stefan Kaes <skaes@railsexpress.de>
2010-02-19 10:18:41 +0100

    we need to give messages some time to run after a rabbit crash

--- lib/beetle/configuration.rb
+++ lib/beetle/configuration.rb
@@ -1,9 +1,10 @@
 module Beetle
   class Configuration
-    attr_accessor :logger, :config_file, :environment
+    attr_accessor :logger, :config_file, :environment, :gc_threshold
     def initialize
       self.logger = Logger.new(STDOUT)
       self.environment = ENV['RAILS_ENV'] || "development"
+      self.gc_threshold = 3.days
     end
   end
 end
