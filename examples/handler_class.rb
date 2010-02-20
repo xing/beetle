93f286d9b8ef5f5013c76a6e4b3eae20598af75d
Stefan Kaes <skaes@railsexpress.de>
2010-02-20 08:30:17 +0100

    demostrating handler classes

---
+++ examples/handler_class.rb
@@ -0,0 +1,42 @@
+# Using Beetle::Handler classes
+require "rubygems"
+require File.expand_path(File.dirname(__FILE__)+"/../lib/beetle")
+
+# setup
+Beetle.config.logger.level = Logger::INFO
+client = Beetle::Client.new
+client.register_queue("test")
+client.register_message("test")
+
+# store message handler results in a redis instance
+STORE = Redis.new(:db => 5)
+KEY = UUID4R::uuid(1)
+
+# declare a handler class fro message processing
+class Handler < Beetle::Handler
+  def process
+    i = message.data.to_i
+    logger.info "adding #{i}"
+    STORE.incr(KEY,i)
+  end
+end
+
+client.register_handler("test", {}, Handler)
+
+# publish some test messages
+N = 10
+n = 0
+N.times {|i| n += client.publish("test", i) }
+puts "published #{n} test messages"
+
+client.listen do
+  EM.add_timer(0.1) { EM.stop_event_loop }
+end
+
+# retrieve processing result and clean redis
+result = STORE.get(KEY)
+STORE.del KEY
+
+puts "Result: #{result}"
+
+raise "something is fishy" unless result = N*(N-1)/2
--- examples/simple.rb
+++ examples/simple.rb
@@ -3,22 +3,18 @@ require "rubygems"
 require File.expand_path(File.dirname(__FILE__)+"/../lib/beetle")
 
 # suppress debug messages
-
 Beetle.config.logger.level = Logger::INFO
 
 # instantiate a client
-
 client = Beetle::Client.new
 
 # register a durable queue named 'test'
 # this implicitly registers a durable topic exchange called 'test'
-
 client.register_queue("test")
 client.register_message("test")
 
 # publish some test messages
 # at this point, the exchange will be created on the server and the queue will be bound to the exchange
-
 n = 0
 n += client.publish("test", "Hello1")
 n += client.publish("test", "Hello2")
@@ -27,7 +23,6 @@ puts "published #{n} test messages"
 puts
 
 # register a handler for the test message, listing on queue "test" with routing key "test"
-
 k = 0
 client.register_handler("test", {}) do |m|
   k += 1
@@ -40,11 +35,9 @@ end
 # start listening
 # this starts the event machine event using EM.run
 # the block passed to listen will be yielded as the last step of the setup process
-
 client.listen do
   EM.add_timer(0.1) { EM.stop_event_loop }
 end
 
 puts "Received #{k} test messages"
-
 raise "Your setup is borked" if n != k
