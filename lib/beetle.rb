3ad39723813a6f72d7552ead03886e044741a0df
Pascal Friederich <pascal.friederich@xing.com>
2010-03-15 18:29:20 +0100

    Allow both the Beetle.config.some_value and the Beetle.config do |c| version for the configuration object

--- lib/beetle.rb
+++ lib/beetle.rb
@@ -36,6 +36,11 @@ module Beetle
   # returns the configuration object
   def self.config
     @config ||= Configuration.new
+    if block_given?
+      yield @config
+    else
+      @config
+    end
   end
 
   # FIXME: there should be a better way to test
