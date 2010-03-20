081a927218f9862e854d64a6f95474b4203fbc3f
Stefan Kaes <skaes@railsexpress.de>
2010-03-20 11:20:54 +0100

    don't access redis when processing non redundadt messages with attempts limit 1

--- examples/rpc.rb
+++ examples/rpc.rb
@@ -13,29 +13,33 @@ client = Beetle::Client.new
 client.register_queue(:echo)
 client.register_message(:echo)
 
-# register a handler for the test message, listing on queue "test" with routing key "test"
-# echoing all data sent to it
-client.register_handler(:echo) do |m|
-  # send data back to publisher
-  m.data
-end
-
-# start the subscriber in a separate thread
-Thread.new do
-  client.listen
-end
-
-n = 100
-ms = Benchmark.ms do
-  n.times do |i|
-    content = "Hello #{i}"
-    # puts "performing RPC with message content '#{content}'"
-    status, result = client.rpc(:echo, content)
-    # puts "status  #{status}"
-    # puts "result  #{result}"
-    # puts
-    $stderr.puts "processing failure for message '#{content}'" if result != content
+if ARGV.include?("--server")
+  # register a handler for the test message, listing on queue "test" with routing key "test"
+  # echoing all data sent to it
+  client.register_handler(:echo) do |m|
+    # send data back to publisher
+    m.data
   end
+
+  # start the subscriber
+  client.listen do
+    puts "started echo server"
+    trap("INT") { puts "stopped echo server"; client.stop_listening }
+  end
+else
+  n = 100
+  ms = Benchmark.ms do
+    n.times do |i|
+      content = "Hello #{i}"
+      # puts "performing RPC with message content '#{content}'"
+      status, result = client.rpc(:echo, content)
+      # puts "status  #{status}"
+      # puts "result  #{result}"
+      # puts
+      $stderr.puts "processing failure for message '#{content}'" if result != content
+    end
+  end
+  printf "Runtime: %dms\n", ms
+  printf "Milliseconds per RPC: %.1f\n", ms/n
 end
-printf "Runtime: %dms\n", ms
-printf "Milliseconds per RPC: %.1f\n", ms/n
+
