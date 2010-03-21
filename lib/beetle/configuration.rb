dbb6479ac9342508f3504abc316cb402f86771ec
Stefan Kaes <skaes@railsexpress.de>
2010-03-21 13:37:30 +0100

    first hackish implementation of redis failover

--- lib/beetle/configuration.rb
+++ lib/beetle/configuration.rb
@@ -5,7 +5,7 @@ module Beetle
     # number of seconds after which keys are removed form the message deduplification store (defaults to <tt>3.days</tt>)
     attr_accessor :gc_threshold
     # the machine where the deduplification store lives (defaults to <tt>"localhost"</tt>)
-    attr_accessor :redis_host
+    attr_accessor :redis_hosts
     # redis database number to use for the message deduplification store (defaults to <tt>4</tt>)
     attr_accessor :redis_db
     # list of amqp servers to use (defaults to <tt>"localhost:5672"</tt>)
@@ -14,7 +14,7 @@ module Beetle
     def initialize #:nodoc:
       self.logger = Logger.new(STDOUT)
       self.gc_threshold = 3.days
-      self.redis_host = "localhost"
+      self.redis_hosts = "localhost:6379"
       self.redis_db = 4
       self.servers = "localhost:5672"
     end
