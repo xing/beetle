153d59255d4060bdd1ae91e0c38373f1726f5e4b
ali.jelveh <ali.jelveh@xing.com>
2010-05-17 18:21:40 +0200

    starting the example and the config-client

--- lib/beetle/redis_configuration_client.rb
+++ lib/beetle/redis_configuration_client.rb
@@ -1,6 +1,39 @@
 require 'timeout'
 module Beetle
   class RedisConfigurationClient < Beetle::Handler
-  
+
+    @@active_master         = nil
+    @@client                = Beetle::Client.new
+    cattr_accessor :active_master
+    cattr_accessor :client
+
+    class << self
+      def find_active_server(master, slave)
+        client.config.redis_configuration_master_retries.times do
+          sleep client.config.redis_configuration_master_retry_timeout.to_i
+          # 
+        end
+      end
+
+      def is_valid_master(redis)
+        redis.info[:]
+      end
+
+      def is_valid_slave(redis)
+        redis.info[:]
+      end
+
+      # DUP
+      def reachable?(redis)
+        begin
+          Timeout::timeout(5) {
+            !!redis.info
+          }
+        rescue Timeout::Error => e
+          false
+        end
+      end
+
+    end
   end
 end
