462d3a30c8636693745c65b3dac5d22173d08ecb
Sebastian Roebke <sebastian.roebke@xing.com>
2010-05-20 19:45:20 +0200

    Working on setting a new redis master

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../lib/beetle')
 
+# Allow using Test::Unit for step assertions
+# See http://wiki.github.com/aslakhellesoy/cucumber/using-testunit
+require 'test/unit/assertions'
+World(Test::Unit::Assertions)
+
+Before do
+  $PIDS_TO_KILL = []
+end
+
 After do
   RedisTestServer.stop_all
+  $PIDS_TO_KILL.each{ |pid| `kill #{pid}` }
 end
