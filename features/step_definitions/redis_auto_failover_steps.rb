bd739a579ae320dde1d85c8c6751ab7790ec0fa1
Sebastian Roebke <sebastian.roebke@xing.com>
2010-05-20 11:01:26 +0200

    Provide a shortcut method to access the redis test servers

--- features/step_definitions/redis_auto_failover_steps.rb
+++ features/step_definitions/redis_auto_failover_steps.rb
@@ -1,31 +1,29 @@
 Given /^a redis server "([^\"]*)" exists as master$/ do |redis_name|
-  redis_server = RedisTestServer.find_or_initialize_by_name(redis_name)
-  redis_server.start
-  redis_server.master
+  RedisTestServer[redis_name].start
+  RedisTestServer[redis_name].master
 end
 
 Given /^a redis server "([^\"]*)" exists as slave of "([^\"]*)"$/ do |redis_name, redis_master_name|
-  redis_server = RedisTestServer.find_or_initialize_by_name(redis_name)
-  redis_server.start
-  redis_server.slave_of(RedisTestServer.find_or_initialize_by_name(redis_master_name).port) 
+  RedisTestServer[redis_name].start
+  RedisTestServer[redis_name].slave_of(RedisTestServer[redis_master_name].port) 
 end
 
 Given /^a redis configuration server using redis servers "([^\"]*)" exists$/ do |redis_server_names|
   redis_servers_string = redis_server_names.split(",").map do |redis_name|
-    RedisTestServer.find_or_initialize_by_name(redis_name).ip_with_port
+    RedisTestServer[redis_name].ip_with_port
   end.join(",")
   `ruby bin/redis_configuration_server --redis-servers=#{redis_servers_string} --redis-retry-timeout 1 > /dev/null 2>&1 &`
 end
 
 Given /^a redis configuration client "([^\"]*)" using redis servers "([^\"]*)" exists$/ do |redis_configuration_client_name, redis_server_names|
   redis_servers_string = redis_server_names.split(",").map do |redis_name|
-    RedisTestServer.find_or_initialize_by_name(redis_name).ip_with_port
+    RedisTestServer[redis_name].ip_with_port
   end.join(",")
   fork {`ruby bin/redis_configuration_client --redis-servers=#{redis_servers_string} > /dev/null 2>&1 &`}
 end
 
 Given /^redis server "([^\"]*)" is down$/ do |redis_name|
-  RedisTestServer.find_or_initialize_by_name(redis_name).stop
+  RedisTestServer[redis_name].stop
 end
 
 Given /^the retry timeout for the redis master check is reached$/ do
