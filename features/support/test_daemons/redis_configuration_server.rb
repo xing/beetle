9097573848ffadeb6e9de5692346f416e557f900
Pascal Friederich <paukul@gmail.com>
2010-06-07 18:40:19 +0200

    Don't use the client-ids param in case there are no clients

--- features/support/test_daemons/redis_configuration_server.rb
+++ features/support/test_daemons/redis_configuration_server.rb
@@ -20,14 +20,17 @@ module TestDaemons
     end
 
     def self.daemon_controller
-      @@daemon_controller ||= DaemonController.new(
-         :identifier    => "Redis configuration test server",
-         :start_command => "ruby bin/redis_configuration_server start -- --redis-servers=#{@@redis_servers} --client-ids=#{@@redis_configuration_clients} --redis-retry-timeout 1",
-         :ping_command  => lambda{ true },
-         :pid_file      => pid_file,
-         :log_file      => log_file,
-         :timeout       => 5
-      )
+      @@daemon_controller ||= begin
+        clients_parameter_string = @@redis_configuration_clients.blank? ? "" : "--client-ids=#{@@redis_configuration_clients}"
+        DaemonController.new(
+           :identifier    => "Redis configuration test server",
+           :start_command => "ruby bin/redis_configuration_server start -- --redis-servers=#{@@redis_servers} #{clients_parameter_string} --redis-retry-timeout 1",
+           :ping_command  => lambda{ true },
+           :pid_file      => pid_file,
+           :log_file      => log_file,
+           :timeout       => 5
+        )
+      end
     end
     
     def self.pid_file
