375b82edd717a40355c27c8e173385b02af855cb
Sebastian Roebke <sebastian.roebke@xing.com>
2010-06-08 18:19:41 +0200

    Use current configuration everytime the configuration server test daemon is started

--- features/support/test_daemons/redis_configuration_server.rb
+++ features/support/test_daemons/redis_configuration_server.rb
@@ -6,31 +6,30 @@ module TestDaemons
     # At the moment, we need only one, so we implement the methods
     # as class methods
 
-    @@started = false
-
+    @@redis_servers = ""
+    @@redis_configuration_clients = ""
+    
     def self.start(redis_servers, redis_configuration_clients)
+      stop
       @@redis_servers = redis_servers
       @@redis_configuration_clients = redis_configuration_clients
       daemon_controller.start
-      @@started = true
     end
 
     def self.stop
-      daemon_controller.stop if @@started
+      daemon_controller.stop
     end
 
     def self.daemon_controller
-      @@daemon_controller ||= begin
-        clients_parameter_string = @@redis_configuration_clients.blank? ? "" : "--client-ids=#{@@redis_configuration_clients}"
-        DaemonController.new(
-           :identifier    => "Redis configuration test server",
-           :start_command => "ruby bin/redis_configuration_server start -- --redis-servers=#{@@redis_servers} #{clients_parameter_string} --redis-retry-timeout 1",
-           :ping_command  => lambda{ true },
-           :pid_file      => pid_file,
-           :log_file      => log_file,
-           :timeout       => 5
-        )
-      end
+      clients_parameter_string = @@redis_configuration_clients.blank? ? "" : "--client-ids #{@@redis_configuration_clients}"
+      DaemonController.new(
+         :identifier    => "Redis configuration test server",
+         :start_command => "ruby bin/redis_configuration_server start -- --redis-servers #{@@redis_servers} #{clients_parameter_string} --redis-retry-timeout 1",
+         :ping_command  => lambda{ true },
+         :pid_file      => pid_file,
+         :log_file      => log_file,
+         :timeout       => 5
+      )
     end
     
     def self.pid_file
