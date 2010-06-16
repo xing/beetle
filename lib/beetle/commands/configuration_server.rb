e83a82c67fca6eba7caeaf01ec4f682aafec451a
Sebastian Roebke <sebastian.roebke@xing.com>
2010-06-16 15:44:03 +0200

    Allow custom pid (and log) directory via --pid-dir parameter when starting configurations daemons

--- lib/beetle/commands/configuration_server.rb
+++ lib/beetle/commands/configuration_server.rb
@@ -6,25 +6,35 @@ module Beetle
   module Commands
     class ConfigurationServer
       def self.execute
-        Daemons.run_proc("redis_configuration_server", :log_output => true) do
-          opts = OptionParser.new 
-          redis_server_strings = []
-          opts.on("-r", "--redis-servers host1:port1,host2:port2,...", String) do |val|
-            redis_server_strings = val.split(",")
-          end
-          opts.on("-c", "--client-ids client-id1,client-id2,...", String) do |val|
-            Beetle.config.redis_configuration_client_ids = val
-          end
-          opts.on("-t", "--redis-retry-timeout seconds", Integer) do |val|
-            Beetle.config.redis_configuration_master_retry_timeout = val
-          end
-          opts.parse!(ARGV - ["start", "--"])
+        opts = OptionParser.new
+        redis_server_strings = []
+        opts.on("-r", "--redis-servers host1:port1,host2:port2,...", String) do |val|
+          redis_server_strings = val.split(",")
+        end
 
-          Beetle.config.servers = "localhost:5672, localhost:5673" # rabbitmq
+        opts.on("-c", "--client-ids client-id1,client-id2,...", String) do |val|
+          Beetle.config.redis_configuration_client_ids = val
+        end
 
-          # set Beetle log level to info, less noisy than debug
-          Beetle.config.logger.level = Logger::INFO
+        opts.on("-t", "--redis-retry-timeout seconds", Integer) do |val|
+          Beetle.config.redis_configuration_master_retry_timeout = val
+        end
 
+        dir_mode = nil
+        dir = nil
+        opts.on("-p", "--pid-dir path", String) do |val|
+          dir_mode = :normal
+          dir = val
+        end
+
+        opts.parse!(ARGV - ["start", "--"])
+
+        Beetle.config.servers = "localhost:5672, localhost:5673" # rabbitmq
+
+        # set Beetle log level to info, less noisy than debug
+        Beetle.config.logger.level = Logger::INFO
+
+        Daemons.run_proc("redis_configuration_server", :log_output => true, :dir_mode => dir_mode, :dir => dir) do
           Beetle::RedisConfigurationServer.new(redis_server_strings).start
         end
       end
