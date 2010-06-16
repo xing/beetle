0c08df5521920bd076dfb62aac31f9a8e840aa13
Sebastian Roebke <sebastian.roebke@xing.com>
2010-06-16 15:44:03 +0200

    Allow custom pid (and log) directory via --pid-dir parameter when starting configurations daemons

--- lib/beetle/commands/configuration_client.rb
+++ lib/beetle/commands/configuration_client.rb
@@ -6,29 +6,36 @@ module Beetle
   module Commands
     class ConfigurationClient
       def self.execute
-        Daemons.run_proc("redis_configuration_client", :multiple => true, :log_output => true) do
-          opts = OptionParser.new 
-          redis_server_strings = []
-          opts.on("-r", "--redis-servers host1:port1,host2:port2,...", String) do |val|
-            redis_server_strings = val.split(",")
-          end
+        opts = OptionParser.new
+        redis_server_strings = []
+        opts.on("-r", "--redis-servers host1:port1,host2:port2,...", String) do |val|
+          redis_server_strings = val.split(",")
+        end
 
-          opts.on("-f", "--redis-master-file path", String) do |val|
-            Beetle.config.redis_master_file = val
-          end
+        opts.on("-f", "--redis-master-file path", String) do |val|
+          Beetle.config.redis_master_file = val
+        end
 
-          client_id = nil
-          opts.on("-i", "--id client-id", String) do |val|
-            client_id = val
-          end
+        client_id = nil
+        opts.on("-i", "--id client-id", String) do |val|
+          client_id = val
+        end
 
-          opts.parse!(ARGV - ["start", "--"])
+        dir_mode = nil
+        dir = nil
+        opts.on("-p", "--pid-dir path", String) do |val|
+          dir_mode = :normal
+          dir = val
+        end
 
-          Beetle.config.servers = "localhost:5672, localhost:5673" # rabbitmq
+        opts.parse!(ARGV - ["start", "--"])
 
-          # set Beetle log level to info, less noisy than debug
-          Beetle.config.logger.level = Logger::INFO
+        Beetle.config.servers = "localhost:5672, localhost:5673" # rabbitmq
 
+        # set Beetle log level to info, less noisy than debug
+        Beetle.config.logger.level = Logger::INFO
+
+        Daemons.run_proc("redis_configuration_client", :multiple => true, :log_output => true, :dir_mode => dir_mode, :dir => dir) do
           client = Beetle::RedisConfigurationClient.new(redis_server_strings)
           client.id = client_id if client_id
           client.start
