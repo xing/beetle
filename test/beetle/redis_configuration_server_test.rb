f45d1f284f34835e872d84573ece046532b0024f
Sebastian Roebke <sebastian.roebke@xing.com>
2010-06-24 10:49:42 +0200

    Intialize message token with current timestamp to allow redis configuration server restarts

--- test/beetle/redis_configuration_server_test.rb
+++ test/beetle/redis_configuration_server_test.rb
@@ -1,17 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 module Beetle
-  class RedisConfigurationServerClientInvalidatedMethodTest < Test::Unit::TestCase 
+  class RedisConfigurationServerClientInvalidatedMethodTest < Test::Unit::TestCase
     test "should ignore outdated client_invalidated messages" do
       Beetle.config.redis_configuration_client_ids = "rc-client-1,rc-client-2"
-      server = RedisConfigurationServer.new([])
+      server = RedisConfigurationServer.new
 
       server.instance_variable_set(:@invalidation_message_token, 2)
       server.client_invalidated("id" => "rc-client-1", "token" => 2)
       old_token = 1.minute.ago.to_f
       server.client_invalidated("id" => "rc-client-2", "token" => 1)
-      
+
       assert_equal({"rc-client-1" => true}, server.instance_variable_get(:@client_invalidated_messages_received))
+    end
+  end
+
+  class RedisConfigurationServerInvalidationMessageTokenTest < Test::Unit::TestCase
+    test "should initialize the invalidation message token with the current timestamp" do
+      now = Time.now
+      Time.stubs(:now).returns(now)
+      server = RedisConfigurationServer.new
+      assert_equal now.to_i, server.invalidation_message_token
     end
   end
 end
