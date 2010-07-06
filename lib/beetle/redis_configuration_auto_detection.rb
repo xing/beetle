8f56b24dcb0f5d3e75ed431a833d13b98183fb2b
Sebastian Roebke <sebastian.roebke@xing.com>
2010-07-06 16:49:22 +0200

    Simplified auto detection

--- lib/beetle/redis_configuration_auto_detection.rb
+++ lib/beetle/redis_configuration_auto_detection.rb
@@ -2,20 +2,18 @@ module Beetle
   module RedisConfigurationAutoDetection #:nodoc:
     # auto detect redis master - will return nil if no valid master detected
     def auto_detect_master
-      return nil unless single_master_reachable? || master_and_slaves_reachable?
+      return nil unless master_and_slaves_reachable?
       redis_instances.find{|r| r.master? }
     end
 
     private
 
-    # whether we have a master slave configuration
-    def single_master_reachable?
-      redis_instances.select{|r| r.master? }.size == 1
-    end
-
-    # can we access both master and slaves
     def master_and_slaves_reachable?
       single_master_reachable? && redis_instances.select{|r| r.slave? }.size == redis_instances.size - 1
+    end
+
+    def single_master_reachable?
+      redis_instances.select{|r| r.master? }.size == 1
     end
   end
 end
