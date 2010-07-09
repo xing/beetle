4c1b03f9850b38451ce85ca69caa33e8a2a5edfc
Sebastian Roebke <sebastian.roebke@xing.com>
2010-07-09 23:51:35 +0200

    rc-server now accepts the master file path via command line option, too

--- features/support/test_daemons/redis_configuration_server.rb
+++ features/support/test_daemons/redis_configuration_server.rb
@@ -8,7 +8,7 @@ module TestDaemons
 
     @@redis_servers = ""
     @@redis_configuration_clients = ""
-    
+
     def self.start(redis_servers, redis_configuration_clients)
       stop
       @@redis_servers = redis_servers
@@ -24,22 +24,26 @@ module TestDaemons
       clients_parameter_string = @@redis_configuration_clients.blank? ? "" : "--client-ids #{@@redis_configuration_clients}"
       DaemonController.new(
          :identifier    => "Redis configuration test server",
-         :start_command => "ruby bin/beetle configuration_server start -- -v --redis-servers #{@@redis_servers} #{clients_parameter_string} --redis-retry-timeout 1 --pid-dir #{tmp_path} --amqp-servers 127.0.0.1:5672",
+         :start_command => "ruby bin/beetle configuration_server start -- -v --redis-master-file #{redis_master_file} --redis-servers #{@@redis_servers} #{clients_parameter_string} --redis-retry-timeout 1 --pid-dir #{tmp_path} --amqp-servers 127.0.0.1:5672",
          :ping_command  => lambda{ true },
          :pid_file      => pid_file,
          :log_file      => log_file,
          :start_timeout => 5
       )
     end
-    
+
+    def self.redis_master_file
+      "#{tmp_path}/redis-master-rc-server"
+    end
+
     def self.pid_file
       "#{tmp_path}/redis_configuration_server.pid"
     end
-    
+
     def self.log_file
       "#{tmp_path}/redis_configuration_server.output"
     end
-    
+
     def self.tmp_path
       File.expand_path(File.dirname(__FILE__) + "/../../../tmp")
     end
