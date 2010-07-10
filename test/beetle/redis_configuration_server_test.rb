f6814c956212262046eeb4db7341f9e7ce5713a5
Stefan Kaes <skaes@railsexpress.de>
2010-07-10 13:17:49 +0200

    nearly C0 coverage for redis_configuration_server

--- test/beetle/redis_configuration_server_test.rb
+++ test/beetle/redis_configuration_server_test.rb
@@ -38,6 +38,27 @@ module Beetle
 
       assert_equal(["rc-client-1"].to_set, @server.instance_variable_get(:@client_pong_ids_received))
     end
+
+    test "the dispatcher should just forward messages to the server" do
+      dispatcher_class = RedisConfigurationServer.class_eval "MessageDispatcher"
+      dispatcher_class.configuration_server = @server
+      dispatcher = dispatcher_class.new
+      payload = {"token" => 1}
+      dispatcher.stubs(:message).returns(stub(:data => payload.to_json, :header => stub(:routing_key=> "pong")))
+      @server.expects(:pong).with(payload)
+      dispatcher.send(:process)
+    end
+
+    test "if a new master is available, it should be published and the available slaves should be configured" do
+      redis = Redis.new
+      other_master = Redis.new(:port => 6380)
+      other_master.expects(:slave_of!).with(redis.host, redis.port)
+      @server.stubs(:redis_master).returns(redis)
+      @server.instance_variable_set(:@redis_server_info, {"master" => [redis, other_master], "slave" => [], "unknown" => []})
+      payload = @server.send(:payload_with_current_token, {"server" => redis.server})
+      @server.beetle.expects(:publish).with(:reconfigure, payload)
+      @server.master_available
+    end
   end
 
   class RedisConfigurationServerInvalidationTest < Test::Unit::TestCase
@@ -72,6 +93,58 @@ module Beetle
       EM::Timer.expects(:new).yields
       @server.master_unavailable
       assert !@server.paused?
+    end
+
+    test "should invalidate the current master after receiving all pong messages" do
+      EM::Timer.expects(:new).yields.returns(:timer)
+      @server.beetle.expects(:publish).with(:invalidate, anything)
+      @server.expects(:cancel_invalidation)
+      @server.expects(:redeem_token).with(1).twice.returns(true)
+      @server.pong("token" => 1, "id" => "rc-client-1")
+      @server.pong("token" => 1, "id" => "rc-client-2")
+    end
+
+    test "should switch the current master after receiving all client_invalidated messages" do
+      @server.expects(:redeem_token).with(1).twice.returns(true)
+      @server.expects(:switch_master)
+      @server.client_invalidated("token" => 1, "id" => "rc-client-1")
+      @server.client_invalidated("token" => 1, "id" => "rc-client-2")
+    end
+
+    test "should switch the current master immediately if there are no clients" do
+      @server.instance_variable_set :@client_ids, Set.new
+      @server.expects(:switch_master)
+      @server.master_unavailable
+    end
+
+    test "switching the master should turn the new master candidate into a master" do
+      new_master = stub(:master! => nil, :server => "jo:6379")
+      @server.beetle.expects(:publish).with(:system_notification, anything)
+      @server.expects(:detect_new_redis_master).returns(new_master)
+      @server.send :switch_master
+      assert_equal new_master, @server.redis_master
+    end
+
+    test "switching the master should resort to the old master if no candidate can be found" do
+      old_master = @server.redis_master
+      @server.beetle.expects(:publish).with(:system_notification, anything)
+      @server.expects(:detect_new_redis_master).returns(nil)
+      @server.send :switch_master
+      assert_equal old_master, @server.redis_master
+    end
+
+    test "checking the availability of redis servers should publish the available servers as long as the master is available" do
+      @server.expects(:master_available?).returns(true)
+      @server.expects(:master_available)
+      @server.send(:redis_master_watcher).send(:check_availability)
+    end
+
+    test "checking the availability of redis servers should call master_unavailable after trying the specified number of times" do
+      @server.stubs(:master_available?).returns(false)
+      @server.expects(:master_unavailable)
+      watcher = @server.send(:redis_master_watcher)
+      watcher.instance_variable_set :@master_retries, 0
+      watcher.send(:check_availability)
     end
   end
 
@@ -186,18 +259,26 @@ module Beetle
       end
     end
 
+    test "should detect a new redis_master" do
+      not_available_redis_master = build_unknown_redis_stub
+      @redis_slave.expects(:slave_of?).returns(true)
+      @server.instance_variable_set(:@redis_master, not_available_redis_master)
+      @server.instance_variable_set(:@redis_server_info, build_redis_server_info(@redis_slave, not_available_redis_master))
+      assert_equal @redis_slave, @server.send(:detect_new_redis_master)
+    end
+
     private
 
     def build_master_redis_stub
-      stub("redis master", :server => "stubbed_master:0", :available? => true, :master? => true, :slave? => false, :role => "master")
+      stub("redis master", :host => "stubbed_master", :port => 0, :server => "stubbed_master:0", :available? => true, :master? => true, :slave? => false, :role => "master")
     end
 
     def build_slave_redis_stub
-      stub("redis slave", :server => "stubbed_slave:0",  :available? => true, :master? => false, :slave? => true, :role => "slave")
+      stub("redis slave", :host => "stubbed_slave", :port => 0, :server => "stubbed_slave:0",  :available? => true, :master? => false, :slave? => true, :role => "slave")
     end
 
     def build_unknown_redis_stub
-      stub("redis unknown", :server => "stubbed_unknown:0",  :available? => false, :master? => false, :slave? => false, :role => "unknown")
+      stub("redis unknown", :host => "stubbed_unknown", :port => 0, :server => "stubbed_unknown:0",  :available? => false, :master? => false, :slave? => false, :role => "unknown")
     end
 
     def build_redis_server_info(*redis_instances)
