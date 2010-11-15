2b0ec91174427b40c74f615fa03690f8d4529923
Pascal Friederich <pascal.friederich@xing.com>
2010-11-15 16:25:46 +0100

    New Release which checks if the Qrack::Client class has already been patched to use send/receive timeouts

--- lib/ext/qrack/client.rb
+++ lib/ext/qrack/client.rb
@@ -1,27 +1,28 @@
 require 'qrack/client'
 
+unless Qrack::Client.instance_methods.include?("socket_with_reliable_timeout")
+  module Qrack
+    class Client
+      # overwrite the timeout method so that SystemTimer is used
+      # instead the standard timeout.rb: http://ph7spot.com/musings/system-timer
+      delegate :timeout, :to => Beetle::Timer
 
-module Qrack
-  class Client
-    # overwrite the timeout method so that SystemTimer is used
-    # instead the standard timeout.rb: http://ph7spot.com/musings/system-timer
-    delegate :timeout, :to => Beetle::Timer
+      def socket_with_reliable_timeout
+        socket_without_reliable_timeout
 
-    def socket_with_reliable_timeout
-      socket_without_reliable_timeout
-
-      secs   = Integer(CONNECT_TIMEOUT)
-      usecs  = Integer((CONNECT_TIMEOUT - secs) * 1_000_000)
-      optval = [secs, usecs].pack("l_2")
+        secs   = Integer(CONNECT_TIMEOUT)
+        usecs  = Integer((CONNECT_TIMEOUT - secs) * 1_000_000)
+        optval = [secs, usecs].pack("l_2")
       
-      begin
-        @socket.setsockopt Socket::SOL_SOCKET, Socket::SO_RCVTIMEO, optval
-        @socket.setsockopt Socket::SOL_SOCKET, Socket::SO_SNDTIMEO, optval
-      rescue Errno::ENOPROTOOPT
+        begin
+          @socket.setsockopt Socket::SOL_SOCKET, Socket::SO_RCVTIMEO, optval
+          @socket.setsockopt Socket::SOL_SOCKET, Socket::SO_SNDTIMEO, optval
+        rescue Errno::ENOPROTOOPT
+        end
+        @socket
       end
-      @socket
-    end
-    alias_method_chain :socket, :reliable_timeout
+      alias_method_chain :socket, :reliable_timeout
 
+    end
   end
 end
