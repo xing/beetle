7539aa654619e84b4eb39c3f24ec34aaec5ad781
Stefan Kaes <stefan.kaes@xing.com>
2018-02-17 09:46:23 +0100

    bumped beetle go version to 2.1.0

--- go/src/github.com/xing/beetle/version.go
+++ go/src/github.com/xing/beetle/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // BEETLE_VERSION is displayed in the web UI and can be checke using beetle --version.
-const BEETLE_VERSION = "2.0.2"
+const BEETLE_VERSION = "2.1.0"
 
 // ReportVersionIfRequestedAndExit checks os.Args for the string --version,
 // prints the version if found and then exits.
