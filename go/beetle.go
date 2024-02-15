963b5bf3992f54f423103c650b67cea6f42ac440
Julian Knocke <julian.knocke@new-work.se>
2024-02-15 16:53:57 +0100

    Completely remove syscall package

--- go/beetle.go
+++ go/beetle.go
@@ -8,7 +8,6 @@ import (
 	"os/signal"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 
 	// "github.com/davecgh/go-spew/spew"
@@ -236,7 +235,7 @@ var interrupted bool
 
 func installSignalHandler() {
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(c, os.Interrupt, unix.SIGTERM)
 	go func() {
 		<-c
 		logInfo("received TERM signal")
