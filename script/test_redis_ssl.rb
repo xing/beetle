8f71faa025201d119bb04ccfb72a879e40f4b55f
David Krentzlin <david.krentzlin@new-work.se>
2025-02-19 17:07:45 +0100

    Update test file

--- script/test_redis_ssl.rb
+++ script/test_redis_ssl.rb
@@ -4,8 +4,6 @@ require 'beetle'
 
 config = Beetle::Configuration.new
 config.redis_tls = true
-config.redis_tls_ca_file = File.expand_path('docker/tls/test_ca.crt', __dir__)
-config.redis_tls_ca_path = File.expand_path('docker/tls', __dir__)
 config.redis_server = "localhost:6379"
 dedup_store = Beetle::DeduplicationStore.new(config)
 puts dedup_store.ping
