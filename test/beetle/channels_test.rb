4eff601a8473fffb6c9de53cb6a3fa3bbced735f
David Krentzlin <david.krentzlin@new-work.se>
2025-02-25 09:32:09 +0100

    slight test cleanup

--- test/beetle/channels_test.rb
+++ test/beetle/channels_test.rb
@@ -17,10 +17,9 @@ module Beetle
       o2 = Object.new
 
       @channels["key"] = o
-
       assert_nil @channels2["key"]
-      @channels2["key"] = o2
 
+      @channels2["key"] = o2
       refute_equal @channels["key"].object_id, @channels2["key"].object_id
     end
   end
