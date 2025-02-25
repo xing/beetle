a5c3ac34ce3f967eec8b3216351d8e258c879a5c
David Krentzlin <david.krentzlin@new-work.se>
2025-02-25 09:09:23 +0100

    Add comment for the Channels holder

--- lib/beetle/publisher.rb
+++ lib/beetle/publisher.rb
@@ -1,4 +1,11 @@
 module Beetle
+  # Provides a thread-local storage for channels which must not be shared between threads.
+  # The current implmentation claims the :beetle_publisher_channels key in the current thread
+  # and maintains a hash that indexes object_ids to their channels.
+  # This way channels are thread scoped and instance scoped. (Multiple publishers can have distinct channels)
+  # 
+  # Performance: Normally you'd want to have fast access to local variables, and an ideal implementation uses an array, instead
+  # of a hash as the underlying store (see also concurrent-ruby ThreadLocalVar). For our case howeverm, this is good enough.
   class Channels
     def initialize
       Thread.current[:beetle_publisher_channels] ||= {}
