5dd8b3d89af024cf83909e9513bea31037b7a9ca
danylo.goncharov <danylo.goncharov@new-work.se>
2025-03-13 14:41:36 +0100

    one more test

--- test/beetle/client_test.rb
+++ test/beetle/client_test.rb
@@ -128,6 +128,12 @@ module Beetle
       assert_equal({:durable => true, :type => :topic, :queues => []}, @client.exchanges["some_exchange"])
     end
 
+    test "registering a message should set publisher confirms if it is defined" do
+      opts = { "exchange" => "some_exchange",  "publisher_confirms" => true  }
+      @client.register_message("some_message", opts)
+      assert_equal({ :key=>"some_message", :persistent => true, :exchange => "some_exchange", :publisher_confirms => true}, @client.messages["some_message"])
+    end
+
     test "registering a message should not fail if the exchange has already been registered" do
       opts = { "exchange" => "some_exchange" }
       @client.register_exchange("some_exchange")
