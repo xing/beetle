54a7145f1052563b3184f9ac9d39f3697627c42f
David Krentzlin <david.krentzlin@new-work.se>
2025-03-17 15:31:09 +0100

    better tests

--- test/beetle/message_test.rb
+++ test/beetle/message_test.rb
@@ -723,8 +723,6 @@ module Beetle
     def setup
       @store = DeduplicationStore.new
       @store.flushdb
-      @no_store = NoDeduplicationStore.new
-      @no_store.flushdb
     end
 
     test "a handler running longer than the specified timeout should be aborted (when given a float timeout number)" do
@@ -737,18 +735,6 @@ module Beetle
       assert_equal RC::ExceptionsLimitReached, result
     end
 
-    test "[NODEDUP] a handler running longer than the specified timeout should be aborted (when given a float timeout number)" do
-      header = header_with_params({})
-      header.expects(:ack)
-      message = Message.new("somequeue", header, 'foo', :timeout => 0.1, :attempts => 2, :store => @no_store)
-      action = lambda{|*args| while true; end}
-      handler = Handler.create(action)
-      result = message.process(handler)
-      assert_equal RC::ExceptionsLimitReached, result
-    end
-
-
-
     test "a handler running longer than the specified timeout should be aborted (when using active_support seconds)" do
       header = header_with_params({})
       header.expects(:ack)
@@ -758,18 +744,13 @@ module Beetle
       result = message.process(handler)
       assert_equal RC::ExceptionsLimitReached, result
     end
+  end
 
-    test "[NODEDUP] a handler running longer than the specified timeout should be aborted (when using active_support seconds)" do
-      header = header_with_params({})
-      header.expects(:ack)
-      message = Message.new("somequeue", header, 'foo', :timeout => 1.seconds, :attempts => 2, :store => @no_store)
-      action = lambda{|*args| while true; end}
-      handler = Handler.create(action)
-      result = message.process(handler)
-      assert_equal RC::ExceptionsLimitReached, result
+  class HandlerTimeoutWitoutDedupStoreTest < HandlerTimeoutTest
+    def setup
+      @store = NoDeduplicationStore.new
+      @store.flushdb
     end
-
-
   end
 
   class MySQLFailoverTest < Minitest::Test
