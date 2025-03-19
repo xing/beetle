b3b95553ff896b53d14c94c29c54fe6e1bd7ca87
David Krentzlin <david.krentzlin@new-work.se>
2025-03-19 08:45:35 +0100

    Make sure storage is initialized on new thread (#126)

--- test/beetle/channels_test.rb
+++ test/beetle/channels_test.rb
@@ -26,5 +26,44 @@ module Beetle
       @channels2["key"] = o2
       refute_equal @channels["key"].object_id, @channels2["key"].object_id
     end
+
+    test "different threads get their own state" do
+      @channels = Beetle::Channels.new
+      saw_key = nil
+
+      th1 = Thread.new do
+        @channels["key"] = Object.new
+        sleep 1
+      end
+
+      th2 = Thread.new do
+        saw_key = @channels["key"]
+      end
+
+      [th1, th2].each(&:join)
+
+      assert_nil @channels["key"]
+      refute saw_key
+    end
+
+    test "different threads with different instances get their own state" do
+      @channels = Beetle::Channels.new
+      @channels2 = Beetle::Channels.new
+      o = Object.new
+      saw_o = nil
+      o2 = Object.new
+      saw_o2 = nil
+
+      Thread.new do
+        @channels["key"] = o
+        @channels2["key"] = o2
+        saw_o = @channels["key"]
+        saw_o2 = @channels2["key"]
+      end.join
+
+      assert saw_o
+      assert saw_o2
+      refute_equal saw_o.object_id, saw_o2.object_id
+    end
   end
 end
