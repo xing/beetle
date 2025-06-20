8ac8d7ed3b776c53891a1b1483b8bc29f8f9e73b
David Krentzlin <david.krentzlin@new-work.se>
2025-06-20 14:47:16 +0200

    Add tests

--- lib/beetle/publisher_session_error_handler.rb
+++ lib/beetle/publisher_session_error_handler.rb
@@ -1,9 +1,14 @@
 module Beetle
   # A bunny session error handler that handles errors occuring in background threads of bunny
   class PublisherSessionErrorHandler
-    def initialize(logger, publisher, server_name)
-      @creating_thread = Thread.current
-      @raise_in = nil
+    attr_reader :server, :reraise_target
+
+    def initialize(logger, publisher, server_name, session_thread = Thread.current)
+      # the thread which has a reference to the session and this error handler
+      @session_thread = session_thread
+
+      # the thread in which the error will be raised if re-reaise is enabled
+      @reraise_target = nil
       @publisher = publisher
       @server = server_name
 
@@ -15,54 +20,84 @@ module Beetle
     end
 
     def exceptions?
-      @error_args != nil
+      @error_mutex.synchronize { !@error_args.nil? }
     end
 
-    # Accepts an exception
-    # If this method is called when code is executed ins inside #reraising_errors block
-    # then the exception will be through in the same thread that the block runs.
-    # If this method is called outside of the #rereaising_errors block then the error is recoreded
-    # and will be through the next time #reraising_errors is called.
+    def reraise_errors?
+      @reraise_errors
+    end
+
+    # Raise is called when a bunny component wants to signal an error
+    # This method will mostly be called from a background thread of bunny.
+    # The logic of this method depends on the value of @reraise_errors.
+    # If @reraise_errors is true, the error will be raised in the thread that is bound to @reraise_target.
+    # @reraise_target is set when the session error handler is used in a block that calls `synchronize_errors`.
     #
-    # This essentially delays the raise of excpeptions in the main thread until the code that is prepared to handle them is executed.
+    # In effect, unless @reraise_errors is true, this will delay throwing of the error until the next call to `synchronize_errors`.
+    #
+    # @param args [Array] the arguments to raise, usually an exception class and a message
     def raise(*args)
       current_thread = Thread.current
       @logger.error "Beetle: bunny session handler errror. server=#{@server} reraise=#{@reraise_errors} raised_in=#{current_thread.inspect}."
 
-      # reraise in thread that created the session?
-      @raise_in.raise(*args) if @reraise_errors && @raise_in
-
-      # don't reraise but record
-      @error_mutex.synchronize { @error_args = args }
-
-      # finally schedule the thread that caused the exception for termination
-      current_thread.kill if current_thread != @creating_thread
+      deliver_to_reraise_target!(*args)
+      record_and_terminate_thread!(current_thread, *args)
     end
 
-    # Executes block which has to be prepared to handle errors from this session error handler
-    # If the error handler has an exeption recorded it will be raised before the block is called
-    # If the error handler does not have an exception recorded it will execute block, and every exception handled during execution
-    # of the block, including those raised from another thread, will be re-raised here.
-    def reraising_errors(&block)
-      reraise_last_error!
+    # This is the main method to surface exceptions that might occur in another thread to the thread that runs this method.
+    # Make sure that you wrap this with error handling code, that can deal with all errors that might have been signaled to the error handler.
+    #
+    # The error semantics are as follows:
+    #
+    # 1. If the error handler has recorded an error, it will raise the last recorded error in `Thread.current`
+    # 2. If no error has been recorded before, it will execute `block` and reraise all exceptions (including those coming from background threads)
+    #    while `block` is executing.
+    #
+    # This method is not thread-safe, so it should only be called from the same thread that has a reference to this error handler.
+    #
+    # Example usage:
+    #
+    # ```ruby
+    #
+    # begin
+    #   my_error_handler.synchronize_errors do
+    #     # run publishing code
+    #   end
+    # rescue Bunny::Exception => e
+    #   # Handle all errors, those in Thread.current but also those that were raised in background threads.
+    #   puts "Bunny error: #{e.message}"
+    # end
+    #
+    # ```
+    def synchronize_errors
       @reraise_errors = true
-      @raise_in = Thread.current
-      block.call
+      @reraise_target = Thread.current
+      flush_pending_errors!
+      yield if block_given?
     ensure
       @reraise_errors = false
-      @raise_in = nil
+      @reraise_target = nil
     end
 
     private
 
-    def reraise_last_error!
-      return unless @error_args
-
-      error = @error_args
-      @error_mutex.synchronize { @error_args = nil }
-
-      Kernel.raise(*error)
+    def deliver_to_reraise_target!(*args)
+      @reraise_target.raise(*args) if @reraise_errors && @reraise_target
     end
 
+    def record_and_terminate_thread!(thread, *args)
+      @error_mutex.synchronize { @error_args = args }
+      thread.kill if thread != @session_thread
+    end
+
+    def flush_pending_errors!
+      error = @error_mutex.synchronize do
+        err = @error_args
+        @error_args = nil
+        err
+      end
+
+      Kernel.raise(*error) if error
+    end
   end
 end
---
+++ test/beetle/publisher_session_error_handler_test.rb
@@ -0,0 +1,122 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+module Beetle
+  class PublisherSessionErrorHandlerTest < Minitest::Test
+    class DummyPublisher; end
+    BackgroundError = Class.new(StandardError)
+    ForgroundError = Class.new(StandardError)
+    DelayedError = Class.new(StandardError)
+
+    def setup
+      @logger_output = StringIO.new
+      @logger = Logger.new(@logger_output)
+      @publisher = DummyPublisher.new
+      @handler = Beetle::PublisherSessionErrorHandler.new(@logger, @publisher, "test-server")
+    end
+
+    def test_synchronize_errors_with_recorded_errors_from_background_thread
+      background = Thread.new do
+        @handler.raise(BackgroundError.new("Background error"))
+      end
+
+      sleep 0.5 # ensure that error is recorded while reraising is still false
+
+      assert_raises(BackgroundError) do
+        @handler.synchronize_errors do
+          assert false, "synchronize_errors was expected to raise previously recorded error"
+        end
+      end
+
+      background.join
+    end
+
+    def test_synchronize_errors_with_errors_from_background_thread_during_reraising
+      background = Thread.new do
+        sleep 0.5 # give the main thread time to call synchronize_errors
+        @handler.raise(BackgroundError.new("Background error"))
+      end
+
+      assert_raises(BackgroundError) do
+        @handler.synchronize_errors do
+          sleep 0.7 # wait for the background thread to raise
+
+          assert false, "synchronize_errors was expected to raise"
+        end
+      end
+
+      background.join
+    end
+
+    def test_synchronize_errors_with_errors_from_main_thread
+      assert_raises(ForgroundError) do
+        @handler.synchronize_errors do
+          raise ForgroundError.new("Foreground error")
+        end
+      end
+    end
+
+    def test_synchronize_errors_with_no_errors_succeeds
+      assert_nothing_raised do
+        result = @handler.synchronize_errors do
+          "No errors here"
+        end
+
+        assert_equal "No errors here", result
+      end
+    end
+
+    def test_records_exception_if_not_reraising
+      @handler.raise(DelayedError.new("Delayed error"))
+      assert @handler.exceptions?
+
+      assert_raises(DelayedError) do
+        @handler.synchronize_errors { } # should raise immediately
+      end
+    end
+
+    def test_does_not_reraise_without_reraising_context
+      assert_nothing_raised do
+        @handler.raise(DelayedError.new("Should not raise"))
+        assert @handler.exceptions?
+      end
+    end
+
+    def test_reraising_errors_resets_state
+      @handler.raise(BackgroundError.new("First"))
+
+      assert_raises(BackgroundError) do
+        @handler.synchronize_errors {}
+      end
+
+      # after that we should not have any recorded exceptions
+      refute @handler.exceptions?
+
+      assert_nothing_raised do
+        @handler.synchronize_errors {}
+      end
+    end
+
+    def test_kills_background_thread
+      bg = Thread.new do
+        @handler.raise(BackgroundError.new("die!"))
+        sleep 0.7
+      end
+
+      sleep 0.3
+      refute bg.alive?
+
+      bg.join(0.2)
+    end
+
+    def test_logs_errors
+      error_message = "Test error"
+      @handler.raise(DelayedError.new(error_message))
+
+      assert_raises(DelayedError) do
+        @handler.synchronize_errors {}
+      end
+
+      assert_match(/Beetle: bunny session handler errror. server=test-server reraise=false/, @logger_output.string)
+    end
+  end
+end
