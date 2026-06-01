0ff97dbacba8f3439edb95191261bf098f593bbe
david-krentzlin <david.krentzlin@new-work.se>
2026-06-01 11:53:17 +0200

    bump amq-protocol

---
+++ .beetle-tabspaces-session.el
@@ -0,0 +1,11 @@
+;; -*- mode: emacs-lisp; lexical-binding:t; coding: utf-8-emacs; -*-
+;; -------------------------------------------------------------------------
+;; Tabspaces Session File for Emacs
+;; -------------------------------------------------------------------------
+;; Created Thu May 21 11:51:24 2026
+
+;; Project to tab name mapping:
+(setq tabspaces-project-tab-map '(("~/code/github/xing/beetle/" . "beetle")))
+
+;; Tab and buffers:
+(setq tabspaces--session-list '((("/Users/david.krentzlin/code/github/xing/beetle/.git/hooks/applypatch-msg.sample" (:kind dired :dir "/Users/david.krentzlin/code/github/xing/beetle/.git/hooks/" :name "hooks") (:kind dired :dir "/Users/david.krentzlin/code/github/xing/beetle/.git/" :name ".git") (:kind dired :dir "/Users/david.krentzlin/code/github/xing/beetle/" :name "beetle")) "beetle" (((min-height . 4) (min-width . 10) (min-height-ignore . 3) (min-width-ignore . 4) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 68) (min-pixel-width . 80) (min-pixel-height-ignore . 51) (min-pixel-width-ignore . 32) (min-pixel-height-safe . 17) (min-pixel-width-safe . 16)) leaf (pixel-width . 3436) (pixel-height . 1372) (total-width . 430) (total-height . 80) (normal-height . 1.0) (normal-width . 1.0) (buffer "applypatch-msg.sample" (selected . t) (hscroll . 0) (fringes 8 8 nil nil) (margins nil) (scroll-bars nil 0 t nil 0 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1)) (prev-buffers ("hooks" 1 235) (".git" 1 229) ("beetle" 1 224) ("*scratch*" 1 1) ("tasks.org" 1 5372))))))
